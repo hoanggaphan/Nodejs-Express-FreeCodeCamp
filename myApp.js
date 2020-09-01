db28d82966a11d0ff0f77be40abc1c891f6ca4cf
hoanggaphan <phansihoang1998@gmail.com>
2020-09-01 13:20:54 +0700

    Initialize App

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -9,22 +9,34 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-
+// console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 
 /** 3) Serve an HTML file */
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+// app.get("/json", (req, res) => {
+//   res.json({"message": "Hello json"});
+// });
 
 /** 6) Use the .env file to configure the app */
- 
+ app.get("/json", (req, res) => {
+  let message = "Hello json";
+  if(process.env.MESSAGE_STYLE == "uppercase") {
+    message = message.toUpperCase();
+  } 
+  res.json({"message": message});
+});
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
